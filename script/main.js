9f1089aff38a3ab96f7914ff7271187724b0ecdf
kaido art <yakubmo165@gmail.com>
2025-05-03 04:45:45 -1200

    changes 8

--- script/main.js
+++ script/main.js
@@ -3,3 +3,38 @@ import { greetUser, addTwoNums } from "./greet.js";
 greetUser("John Doe");
 addTwoNums(20, 23);
 
+// destructuring is getting specific datat from a variable or object for use
+// const student = {
+//   name: "Muhammad",
+//   age: "24",
+//   course: "bscs"
+  
+// }
+// console.log(student);
+// destructuring format
+// const { name, age, course } = student;
+// console.log(`My name is ${name}, I am ${age}, i am studying ${course}`);
+
+// to spread from two variable arrays use ...
+
+const studentt = {
+    name: "Muhammad",
+    age: "24",
+    course: "bscs"
+};
+
+const schooldata = {
+    schoolname: "Abu zaria",
+    Address: "kotangora estate",
+    class: "bsit-2020",
+};
+
+// spread syntax
+const fullinfo = {
+    ...schooldata,
+    ...studentt
+};
+
+const { name, age, schoolname, course, Address } = fullinfo;
+console.log(fullinfo);
+console.log(`My name is ${name}, I am ${age}, i am studying ${course}, i am schooling in ${schoolname}, at location ${Address}`);
